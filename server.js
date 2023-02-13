2b439def96a2c11660fe42c765a0389a813c17aa
meriç karabulut <karabulutmeric1@gmail.com>
2023-02-13 19:06:28 +0300

    routers and controllers added, user model created

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const db = require("./connection");
+const cors = require("cors")
 
+const userRoutes = require("./routes/userRoutes")
+
+const { User } = require("./models/User");
 const app = express();
 const port = 3000;
+
 
 app.get('/', (req, res) => {
   res.send('Hello Get');
 
 });
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/user', userRoutes);
 
 // app.get('/print/:name', (req, res) => {
 
@@ -15,12 +27,6 @@ app.get('/', (req, res) => {
 //     var url = req.baseUrl
 
 // });
-
-app.post('/', (req, res) => {
- 
-    res.send("Hello post")
-});
-
 db.sync()
   .then(() => {
     console.log("Synced db.");
